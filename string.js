158bb3a3bfb8c1617dc319e8a5f95fabcaaf9de3
shji023 <shji0@naver.com>
2021-02-03 21:26:57 +0900

    string

---
+++ string.js
@@ -0,0 +1,5 @@
+let str = "hello world ! ^^ ~~";
+let matchstr = "hello";
+console.log(str.startsWith(matchstr)); // true
+console.log(str.endsWith(matchstr)); // false
+console.log(str.includes(matchstr)); // true
